2f49b889938459079a09a502303fd7a8be1e5dbd
Taylor Price <tayworm@gmail.com>
2024-03-01 16:04:43 -0700

    chore: hello world working, progress towards function calling
    Signed-off-by: Taylor Price <tayworm@gmail.com>

--- main.py
+++ main.py
@@ -4,19 +4,28 @@ from typing import AsyncIterable, Iterable, List, Optional
 
 import google.generativeai as genai
 import openai.types
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse, StreamingResponse
+from fastapi.routing import APIRouter
 from pydantic import BaseModel
 from vertexai.generative_models import (Content, FunctionDeclaration, GenerationResponse, GenerativeModel, Part, Tool)
 
 app = FastAPI()
+router = APIRouter()
 genai.configure(api_key=os.environ['GEMINI_API_KEY'])
+
+
+@app.middleware("http")
+async def log_body(request: Request, call_next):
+    body = await request.body()
+    print("REQUEST BODY: ", body)
+    return await call_next(request)
 
 
 class Message(BaseModel):
     role: str
-    content: str
+    content: str | None = None
 
 
 class Delta(Message):
@@ -62,7 +71,7 @@ class TopLogprob(BaseModel):
 
 
 class OAIContent(BaseModel):
-    token: str
+    token: str | None = None
     logprob: Optional[int] = None
     bytes: Optional[List[int]] = None
     top_logprobs: Optional[List[TopLogprob]] = None
@@ -79,11 +88,18 @@ class Choice(BaseModel):
     logprobs: Logprobs
 
 
+class ToolCall(BaseModel):
+    id: str
+    type: str
+    function: Function
+
+
 class RespChoice(BaseModel):
-    finish_reason: str
+    finish_reason: str | None = None
     index: int
     delta: Delta
     logprobs: Logprobs
+    tool_calls: List[ToolCall] | None = None
 
 
 class Usage(BaseModel):
@@ -102,12 +118,6 @@ class OAICompletionResponse(BaseModel):
     usage: Usage | None = None
 
 
-class ToolCall(BaseModel):
-    id: str
-    type: str
-    function: Function
-
-
 @app.get("/models")
 def list_models():
     models = []
@@ -123,7 +133,7 @@ def list_models():
 @app.post("/chat/completions", response_model_exclude_none=True, response_model_exclude_unset=True,
           response_model=OAICompletionResponse)
 async def chat_completion(data: OAICompletionRequest):
-    print("GPTSCRIPT INPUT: ", data)
+    # print("GPTSCRIPT INPUT: ", data)
     if data.tools is not None:
         gtool_func_declarations = []
         for tool in data.tools:
@@ -149,7 +159,7 @@ async def chat_completion(data: OAICompletionRequest):
         parts=parts
     )
 
-    model = GenerativeModel("gemini-pro")
+    model = GenerativeModel("gemini-pro", tools=tools)
     print("CALLING GENERATE CONTENT")
     response = model.generate_content([content], tools=tools, stream=data.stream)
 
@@ -158,9 +168,9 @@ async def chat_completion(data: OAICompletionRequest):
 
 
 async def async_chunk(chunks: Iterable[GenerationResponse]) -> AsyncIterable[str]:
-    print("CHUNKS FROM GEMINI: ")
+    # print("CHUNKS FROM GEMINI: ")
     for chunk in chunks:
-        print(chunk)
+        #         print(chunk)
         chunk = map_gemini_resp(chunk)
         yield "data: " + chunk.json() + "\n\n"
 
@@ -171,20 +181,63 @@ def _generate_id():  # private helper function
 
 def map_gemini_resp(response: GenerationResponse) -> OAICompletionResponse:
     choices = []
-    for item in response.candidates:
+    print("PRINTING GEMINI RESPONSE AS DICT: ", response.to_dict())
+    response = response.to_dict()
+    for idx, item in enumerate(response["candidates"]):
+        # print("ITEM: ", item)
+        # tool_calls = [
+        #     {
+        #         "id": "some_thing",  # only on initial tool call?
+        #         "index": idx,
+        #         "type": "function_call",  # only on initial tool call?
+        #         "function": {
+        #             "name": item["content"]["parts"][0]["function_call"]["name"],
+        #             "arguments": item["content"]["parts"][0]["function_call"]["args"]["question"]
+        #         }
+        #     }
+        # ]
+        # print("MAPPED TOOL CALLS: ", tool_calls)
+
+        try:
+            tool_calls = [
+                {
+                    "id": "some_thing",  # only on initial tool call?
+                    "index": idx,
+                    "type": "function",  # only on initial tool call? function_call or function?
+                    "function": {
+                        "name": item["content"]["parts"][0]["function_call"]["name"],
+                        "arguments": item["content"]["parts"][0]["function_call"]["question"]
+                    }
+                }
+            ]
+        except:
+            tool_calls = None
+
+        try:
+            finish_reason = 'continue'
+            # finish_reason = map_finish_reason(item["finish_reason"])
+        except KeyError:
+            finish_reason = None
+
+        try:
+            content = item["content"]["parts"][0]["text"]
+        except KeyError:
+            content = None
+
         choice = RespChoice(
-            finish_reason=map_finish_reason(item.finish_reason.name),
-            index=item.index,
+            finish_reason=finish_reason,
+            index=idx,
             delta=Delta(
-                role=item.content.role,
-                content=item.content.parts[0].text
+                role=item["content"]["role"],
+                content=content
             ),
             logprobs=Logprobs(
                 content=[
                     OAIContent(
-                        token=item.content.parts[0].text,
+                        token=content,
                     )
-                ]
+                ],
+                tool_calls=tool_calls
             )
         )
         choices.append(choice)
@@ -205,7 +258,8 @@ def map_gemini_resp(response: GenerationResponse) -> OAICompletionResponse:
     )
 
 
-def map_finish_reason(finish_reason: str):
+def map_finish_reason(finish_reason: str) -> str:
+    print("FINISH REASON: ", finish_reason)
     # openai supports 5 stop sequences - 'stop', 'length', 'function_call', 'content_filter', 'null'
     if (finish_reason == "ERROR"):
         return "stop"
@@ -214,5 +268,7 @@ def map_finish_reason(finish_reason: str):
     elif finish_reason == "SAFETY":  # vertex ai
         return "content_filter"
     elif finish_reason == "STOP":  # vertex ai
+        return "stop"
+    elif finish_reason == 1:  # vertex ai
         return "stop"
     return finish_reason
